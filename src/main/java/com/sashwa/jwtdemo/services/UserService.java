9e7faa5d2f1ac6422a8af62a9227d29ec6b50b3f
Ashwani Singh <ashwanikumarsingh@outlook.com>
2024-07-22 23:46:54 +0530

    token based authentication demo using JWT

---
+++ src/main/java/com/sashwa/jwtdemo/services/UserService.java
@@ -0,0 +1,26 @@
+
+package com.sashwa.jwtdemo.services;
+
+import com.sashwa.jwtdemo.entities.User;
+import com.sashwa.jwtdemo.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
